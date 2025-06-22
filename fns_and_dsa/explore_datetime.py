a1f87d9d84f83a76be79134a4be00c586972cbe7
Albert Mithamo <albertmwendia47@gmail.com>
2025-06-22 21:43:52 +0300

    modified datetime file

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timedelta
 def display_current_datetime ():
     current_date = datetime.now()
-    print(current_date);
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(formatted_date);
 
 display_current_datetime()
 
